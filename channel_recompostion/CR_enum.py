65c2c335d17142e0ca9b73754c113fb7f3d4b52e
Armageddon-15 <hjc014@126.com>
2023-03-11 01:01:21 +0800

    Update CR_enum.py

--- channel_recompostion/CR_enum.py
+++ channel_recompostion/CR_enum.py
@@ -1,9 +1,10 @@
 import enum
 
 
-class Channel(enum.Enum):
-    r = 0
-    g = 1
-    b = 2
-    a = 3
-    hint = 9999
+class Channel(enum.IntFlag):
+    r = 0b0001
+    g = 0b0010
+    b = 0b0100
+    a = 0b1000
+    all = 0b1111
+    hint = 0b0000
