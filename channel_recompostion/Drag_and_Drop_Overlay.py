de7218a0483a1480c44a9af27626b53c01192aad
Armageddon-15 <hjc014@126.com>
2023-03-12 00:55:47 +0800

    Update Drag_and_Drop_Overlay.py

--- channel_recompostion/Drag_and_Drop_Overlay.py
+++ channel_recompostion/Drag_and_Drop_Overlay.py
@@ -10,7 +10,7 @@ class DnDWidget(QLabel):
         super().__init__(parent)
         self.data = None
         self.setAcceptDrops(True)
-        self.setStyleSheet("background-color: rgba(0, 0, 0, 180);")
+        self.setStyleSheet("color: rgba(0, 0, 0, 0); background-color: rgba(0, 0, 0, 0);")
 
     def dragEnterEvent(self, e) -> None:
         e.acceptProposedAction()
