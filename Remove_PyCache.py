cda508168bac088de9286fe31d41d7aaf1168a6d
Armageddon-15 <hjc014@126.com>
2023-04-04 02:35:45 +0800

    Create Remove_PyCache.py

---
+++ Remove_PyCache.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+
+dest_dir = r".\dist\ChannelRecomposition"
+
+
+def deleteCache(d):
+    d = os.path.abspath(d)
+    for dirs in os.listdir(d):
+        dirs = os.path.join(d, dirs)
+        if os.path.isdir(dirs):
+            deleteCache(dirs)
+            if os.path.relpath(dirs, d) == "__pycache__":
+                print("remove path", dirs)
+                shutil.rmtree(dirs)
+
+
+deleteCache(dest_dir)
